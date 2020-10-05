10b0b493128a3b9ae309606ac72fb34c0e4e6c22
reddi sharan <reddisharan@gmail.com>
2020-10-05 17:11:48 +0530

    test jest with env

--- components/Title.js
+++ components/Title.js
@@ -25,7 +25,7 @@ export default class Title extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>This is a React Native snapshot</Text>
+        <Text style={styles.instructions}>This is a React Native</Text>
       </View>
     );
   }
