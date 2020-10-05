6d8f3ef1000719c5cbce00281b9ead362b3f9e3f
reddi sharan <reddisharan@gmail.com>
2020-10-05 15:44:39 +0530

    eslint

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-// import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    < View View style={styles.container}>
       <Text>sharan1234454</Text>
       <Text>iam a frontend developer</Text>
       <Text>I love my country</Text>
-      {/* <StatusBar style='auto' /> */}
-    </View>
+    < /View>
   );
 }
 
